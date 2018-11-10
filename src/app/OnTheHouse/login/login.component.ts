36502b9fcdeaaefc450c92e9daa5b9698ae482b8
Natalie Noakes <nnoakes@smu.edu>
2018-11-10 14:36:24 -0600

    signup and login modals basically finished

--- src/app/OnTheHouse/login/login.component.ts
+++ src/app/OnTheHouse/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { User } from '../../domain';
+import { FormControl, ReactiveFormsModule, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl } from '@angular/forms/src/model';
+import { RouterModule, Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public user = new User();
+  changeData: FormGroup;
+  // wantRemember: boolean;
+  public loggedIn: boolean = true;
+
+  constructor(public router: Router, public http: HttpClient) {
+    this.changeData = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)])
+    });
+    this.changeData.valueChanges.subscribe(val => this.update(val));
+
+    // this.wantRemember = false;
+  }
 
   ngOnInit() {
+    // if (localStorage.getItem('email')) {
+    //   this.changeData.get('email').setValue(localStorage.getItem('email'));
+    //   document.getElementById('remember').setAttribute("checked", "");
+    //   this.remember();
+    // }
+  }
+
+  public login() {
+    // if (this.changeData.valid) {
+    //   this.wantRemember ? localStorage.setItem('email', this.user.email) : localStorage.removeItem('email');
+    // }
+
+    this.http.post('http://ec2-18-188-176-205.us-east-2.compute.amazonaws.com/login',
+      {
+        email: this.user.email.toString(),
+        password: this.user.password.toString()
+      }
+    ).subscribe(data => {
+      console.log(data);
+      if (data['session_id'] != 401) {
+        localStorage.setItem('session_id', data['session_id']);
+        this.router.navigate(['/']);
+      }else {
+        this.loggedIn = false;
+      }
+    });
+  }
+
+
+
+
+  public update(user: User) {
+    this.user = user;
   }
 
 }
--- src/app/OnTheHouse/signup/signup.component.ts
+++ src/app/OnTheHouse/signup/signup.component.ts
@@ -16,14 +16,20 @@ export class SignupComponent implements OnInit {
   changeData: FormGroup;
   public success: boolean = false;
   public failure: boolean = false;
+  selectedSchool: string;
+  schools = [
+    {name: "Southern Methodist University", value: 1},
+    {name: "University of North Texas", value: 2},
+    {name: "The University of Texas at Dallas", value: 3}
+  ]
 
   constructor(public router: Router, public http: HttpClient) {
     this.changeData = new FormGroup({
       userName: new FormControl(null, [Validators.required, Validators.minLength(3)]),
       email: new FormControl(null, [Validators.required, Validators.email]),
       reemail: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
-      school: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)])//,
+      //school: new FormControl(null, [Validators.required])
     });
     this.changeData.valueChanges.subscribe(val => this.update(val));
 
@@ -36,10 +42,10 @@ export class SignupComponent implements OnInit {
   public signup() {
     this.http.post('http://ec2-18-188-176-205.us-east-2.compute.amazonaws.com/signup',
       {
-        name: this.newUser.userName.toString(),
+        username: this.newUser.userName.toString(),
         email: this.newUser.email.toString(),
         password: this.newUser.password.toString(),
-        school: this.newUser.school.toString()
+        school: this.selectedSchool
       }
     ).subscribe(data => {console.log(data);
       if (data['valid'] == 0) {
