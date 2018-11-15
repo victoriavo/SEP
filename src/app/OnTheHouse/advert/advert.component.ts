d26dc921ba69c7dcdf6119af9ccc7a524908a2c5
victoriavo <vickivo11@yahoo.com>
2018-11-15 13:31:51 -0600

    hover over arrows colors them

---
+++ src/app/OnTheHouse/advert/advert.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'advert',
+  templateUrl: './advert.component.html',
+  styleUrls: ['./advert.component.css']
+})
+
+export class AdvertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/domain/index.ts
+++ src/app/domain/index.ts
@@ -2,6 +2,7 @@
 export * from './domain.module';
 
 // models
+export * from './models/advert';
 export * from './models/user';
 export * from './models/post';
 
