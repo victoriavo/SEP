c760d8ed666f90b65033cda4b2d75c7f1e7997af
Muaz <muaz-4692682418@Muazs-MacBook-Pro.local>
2018-11-16 00:01:45 -0600

    advert working

--- src/app/OnTheHouse/advert/advert.component.ts
+++ src/app/OnTheHouse/advert/advert.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Advert } from '../../domain';
 
 @Component({
   selector: 'advert',
@@ -8,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 
 export class AdvertComponent implements OnInit {
 
+  public advert : Advert = {};
+
   constructor() { }
 
   ngOnInit() {
+    
+    this.advert.name = "Subway";
+    this.advert.information = "STEAK it up a notch with the all new Chioptle Steak Sandwitch";
+    this.advert.location = "3426 Oak Lawn Avenue";
+    this.advert.link = "https://www.subway.com/en-US/MenuNutrition/Menu";
   }
 
 }
